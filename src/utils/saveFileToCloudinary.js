d138eba5595ce7291187e36921cf92745b58334d
Max <yurchenko.maksym@gmail.com>
2026-04-22 15:49:26 +0100

    final

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,4 +1,4 @@
-import { Readable } from 'node:stream';
+
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
@@ -8,14 +8,15 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export async function saveFileToCloudinary(buffer) {
+export async function saveFileToCloudinary(buffer, userId) {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
       {
         folder: 'notes-app/avatars',
         resource_type: 'image',
+        public_id: `avatar_${userId}`,
         overwrite: true,
-        unique_filename: true,
+        unique_filename: false,
       },
       (error, result) => {
         if (error) {
@@ -27,6 +28,6 @@ export async function saveFileToCloudinary(buffer) {
       },
     );
 
-    Readable.from(buffer).pipe(uploadStream);
+    uploadStream.end(buffer);
   });
-}
+};
